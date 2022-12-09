eaf8e497e01e8c5a29d4203397a37c91d198e6de
Prajwal-ECE <118184797+Prajwal-ECE@users.noreply.github.com>
2022-12-10 00:51:04 +0530

    Add files via upload

---
+++ Day18[D Flipflop]/dff.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Author: Prajwal N
+// Create Date:    00:16:09 12/8/2022  
+// Module Name:    dff 
+// Project Name: D flipflop
+// Tool versions: Xilinx ISE Design Suit 14.7 
+// Revision 0.01 - File Created
+//////////////////////////////////////////////////////////////////////////////////
+module DFF(Q,Qbar,D,Clk,Reset);
+
+output  reg  Q; 
+
+output   Qbar; 
+
+input   D,Clk,Reset; 
+
+assign Qbar = ~Q; 
+
+always @(posedge Clk) 
+
+begin 
+
+ if (Reset == 1'b1) //If not at reset 
+
+  Q = 1'b0;
+
+ else 
+
+  Q = D;
+
+end 
+
+endmodule
---
+++ Day18[D Flipflop]/dff_tb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Author: Prajwal N
+// Create Date:   00:24:29 12/8/2022
+// Design Name:   D flipflop
+// Module Name:   P:/xilin programs/verilog/dff/dff_tb.v
+// Project Name:  D Flipflop 
+// Tool versions:  Xilinx ISE Design Suit 14.7
+// Verilog Test Fixture created by ISE for module: clockdivider
+// Revision 0.01 - File Created
+////////////////////////////////////////////////////////////////////////////////
+module DFF_tb;
+
+ // Inputs
+
+ reg D;
+
+ reg Clk;
+
+ reg Reset;
+
+ // Outputs
+
+ wire Q;
+
+ wire Qbar;
+
+ DFF uut (.Q(Q),.Qbar(Qbar),.D(D),.Clk(Clk),.Reset(Reset));
+
+ initial begin
+ $monitor( " D=%b  Q=%b  Qbar=%b",D,Q,Qbar);
+ end
+ initial begin
+
+  D  = 1'b0;
+
+  Clk  = 1'b0;
+
+  Reset   = 1'b1;
+
+  #100;
+
+  Reset = 1'b0;
+
+  #20;
+
+  forever #40 D = ~ D;
+
+ end
+
+   always #10 Clk = ~Clk;  
+
+endmodule
